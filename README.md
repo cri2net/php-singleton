a3e2618af8d44593e7da090e02bafd88c65f409a
Andreу Davidovich <cri2net@gmail.com>
2016-01-30 01:37:39 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# README #
+
+Это php trait для реализации паттерна Singleton;
+
+## Использование:
+
+```
+<?php
+
+use cri2net\php_singleton\Singleton;
+
+class YourClass
+{
+    use Singleton;
+    
+    /*
+    code of your class
+    */
+}
+```
---
+++ src/Singleton.php
@@ -0,0 +1,45 @@
+<?php
+namespace cri2net\php_singleton;
+
+trait Singleton
+{
+    /**
+     * @var Singleton reference to singleton instance
+     */
+    private static $instance;
+
+    /**
+     * gets the instance via lazy initialization (created on first usage).
+     *
+     * @return self
+     */
+    public static function getInstance()
+    {
+        if (static::$instance === null) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+
+    protected function __construct()
+    {
+    }
+
+    /**
+     * Prevent the instance from being cloned.
+     *
+     * @return void
+     */
+    final private function __clone()
+    {
+    }
+
+    /**
+     * Prevent from being unserialized.
+     *
+     * @return void
+     */
+    final private function __wakeup()
+    {
+    }
+}
